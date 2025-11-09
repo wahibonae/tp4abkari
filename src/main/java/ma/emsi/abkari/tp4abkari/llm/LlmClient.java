b4b75c9dc9ab632d154ae6cf304bc80a29952ffe
wahibonae <wahibabkari@gmail.com>
2025-11-09 23:31:36 +0100

    add: rag naif

--- src/main/java/ma/emsi/abkari/tp4abkari/llm/LlmClient.java
+++ src/main/java/ma/emsi/abkari/tp4abkari/llm/LlmClient.java
@@ -12,20 +12,18 @@ import dev.langchain4j.data.message.SystemMessage;
 import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
-import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.StreamingChatModel;
 import dev.langchain4j.model.embedding.EmbeddingModel;
-import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiStreamingChatModel;
 import dev.langchain4j.rag.content.retriever.ContentRetriever;
 import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
-import dev.langchain4j.rag.query.router.LanguageModelQueryRouter;
-import dev.langchain4j.rag.query.router.QueryRouter;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.TokenStream;
 import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
 import dev.langchain4j.store.embedding.EmbeddingStore;
 import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+import dev.langchain4j.web.search.WebSearchEngine;
+import dev.langchain4j.web.search.tavily.TavilyWebSearchEngine;
 
 import java.net.URISyntaxException;
 import java.net.URL;
@@ -34,9 +32,7 @@ import java.nio.file.Path;
 import java.nio.file.PathMatcher;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class LlmClient {
     // Clé pour l'API du LLM
@@ -55,23 +51,51 @@ public class LlmClient {
         // Récupère la clé secrète pour travailler avec l'API du LLM, mise dans une variable d'environnement
         // du système d'exploitation.
         this.key = System.getenv("GEMINI_KEY");
+        String tavilyCle = System.getenv("TAVILY_KEY");
 
         if (key == null || key.isEmpty()) {
             System.err.println("❌ ERREUR: La clé GEMINI_KEY n'est pas définie!");
             throw new RuntimeException("La clé API GEMINI_KEY n'est pas configurée");
         }
 
-        ChatModel chatModel = GoogleAiGeminiChatModel.builder()
+        StreamingChatModel chatModel = GoogleAiGeminiStreamingChatModel.builder()
                 .apiKey(key)
                 .modelName("gemini-2.5-flash")
+                .temperature(0.3)
                 .logRequestsAndResponses(true) // Technique Test 2
                 .build();
 
-        this.chatMemory = MessageWindowChatMemory.withMaxMessages(10);
+        URL resourceUrl = LlmClient.class.getResource("/");
+        Path resourcePath = Paths.get(resourceUrl.toURI());
 
-        this.assistant = AiServices.builder(Assistant.class)
-                .streamingChatModel(chatModel)
-                .chatMemory(chatMemory)
+        DocumentParser documentParser = new ApacheTikaDocumentParser();
+
+        List<Document> documents = FileSystemDocumentLoader.loadDocuments(
+                resourcePath,
+                documentParser
+        );
+
+        DocumentSplitter splitter = DocumentSplitters.recursive(300, 50);
+
+        // Split all documents into segments
+        List<TextSegment> allSegments = new ArrayList<>();
+        for (Document document : documents) {
+            List<TextSegment> segments = splitter.split(document);
+            allSegments.addAll(segments);
+        }
+
+        EmbeddingModel embeddingModel = new AllMiniLmL6V2EmbeddingModel();
+
+        List<Embedding> embeddings = embeddingModel.embedAll(allSegments).content();
+
+        EmbeddingStore<TextSegment> embeddingStore = new InMemoryEmbeddingStore<>();
+        embeddingStore.addAll(embeddings, allSegments);
+
+        ContentRetriever pdfRetriever = EmbeddingStoreContentRetriever.builder()
+                .embeddingStore(embeddingStore)
+                .embeddingModel(embeddingModel)
+                .maxResults(3)
+                .minScore(0.5)
                 .build();
     }
 
