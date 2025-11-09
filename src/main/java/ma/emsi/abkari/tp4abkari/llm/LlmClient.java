97a2f321d59f8946d6ca291f5173a18990de2ed7
wahibonae <wahibabkari@gmail.com>
2025-11-09 23:25:24 +0100

    technique test 2: added logging

--- src/main/java/ma/emsi/abkari/tp4abkari/llm/LlmClient.java
+++ src/main/java/ma/emsi/abkari/tp4abkari/llm/LlmClient.java
@@ -1,13 +1,42 @@
 package ma.emsi.abkari.tp4abkari.llm;
 
+import dev.langchain4j.data.document.Document;
+import dev.langchain4j.data.document.DocumentParser;
+import dev.langchain4j.data.document.DocumentSplitter;
+import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
+import dev.langchain4j.data.document.parser.apache.tika.ApacheTikaDocumentParser;
+import dev.langchain4j.data.document.splitter.DocumentSplitters;
+import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.data.message.SystemMessage;
+import dev.langchain4j.data.segment.TextSegment;
 import dev.langchain4j.memory.ChatMemory;
 import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.model.chat.ChatModel;
 import dev.langchain4j.model.chat.StreamingChatModel;
+import dev.langchain4j.model.embedding.EmbeddingModel;
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
 import dev.langchain4j.model.googleai.GoogleAiGeminiStreamingChatModel;
+import dev.langchain4j.rag.content.retriever.ContentRetriever;
+import dev.langchain4j.rag.content.retriever.EmbeddingStoreContentRetriever;
+import dev.langchain4j.rag.query.router.LanguageModelQueryRouter;
+import dev.langchain4j.rag.query.router.QueryRouter;
 import dev.langchain4j.service.AiServices;
 import dev.langchain4j.service.TokenStream;
+import dev.langchain4j.model.embedding.onnx.allminilml6v2.AllMiniLmL6V2EmbeddingModel;
+import dev.langchain4j.store.embedding.EmbeddingStore;
+import dev.langchain4j.store.embedding.inmemory.InMemoryEmbeddingStore;
+
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class LlmClient {
     // Clé pour l'API du LLM
@@ -22,7 +51,7 @@ public class LlmClient {
     // Mémoire de l'assistant pour garder l'historique de la conversation
     private ChatMemory chatMemory;
 
-    public LlmClient() {
+    public LlmClient() throws URISyntaxException {
         // Récupère la clé secrète pour travailler avec l'API du LLM, mise dans une variable d'environnement
         // du système d'exploitation.
         this.key = System.getenv("GEMINI_KEY");
@@ -32,9 +61,10 @@ public class LlmClient {
             throw new RuntimeException("La clé API GEMINI_KEY n'est pas configurée");
         }
 
-        StreamingChatModel chatModel = GoogleAiGeminiStreamingChatModel.builder()
+        ChatModel chatModel = GoogleAiGeminiChatModel.builder()
                 .apiKey(key)
                 .modelName("gemini-2.5-flash")
+                .logRequestsAndResponses(true) // Technique Test 2
                 .build();
 
         this.chatMemory = MessageWindowChatMemory.withMaxMessages(10);
